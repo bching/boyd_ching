e1aac954e41c422385701cd7f64ee11ff16b1d6f
Boyd <chingboyd@gmail.com>
2017-03-24 19:01:59 -0800

    add Mustache template engine

--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -4,19 +4,26 @@ namespace MyProject\Controllers;
 
 use Http\Response;
 use Http\Request;
+use MyProject\Template\Renderer;
 
 class Homepage {
   private $response;
   private $request;
+  private $renderer;
 
-  public function __construct(Request $request, Response $response) {
+  public function __construct(
+    Request $request, Response $response, Renderer $renderer
+  ) {
     $this->request = $request;
     $this->response = $response;
+    $this->renderer = $renderer;
   }
 
   public function show() {
-    $content = '<h1>Hello World</h1>';
-    $content .= 'Hello ' . $this->request->getParameter('name', 'stranger');
-    $this->response->setContent($content);
+    $data = [
+      'name' => $this->request->getParameter('name', 'stranger'),
+        ];
+    $html = $this->renderer->render('Homepage', $data);
+    $this->response->setContent($html);
   }
 }
---
+++ src/Dependencies.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+$injector = new \Auryn\Injector;
+
+// Tells injector class to inject an instance of Http\HttpRequest any time 
+// it encounters an Http\Request type-hint
+$injector->alias('Http\Request', 'Http\HttpRequest');
+// Store Http\HttpRequest in injector's shared cache and all future
+// requests to provider for an injected instance return originally
+// created object
+$injector->share('Http\HttpRequest');
+$injector->define('Http\HttpRequest', [
+  ':get' => $_GET,
+  ':post' => $_POST,
+  ':cookies' => $_COOKIE,
+  ':files' => $_FILES,
+  ':server' => $_SERVER,
+]);
+
+$injector->alias('Http\Response', 'Http\HttpResponse');
+$injector->share('Http\HttpResponse');
+
+$injector->alias('MyProject\Template\Renderer', 'MyProject\Template\MustacheRenderer');
+// Rename .mustache extension to .html for compatibility with other template engines
+// Load Mustache from templates folder in root directory
+$injector->define('Mustache_Engine', [
+  ':options' => [
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__DIR__) . '/templates', [
+    'extension' => '.html',
+    ]),
+  ],
+]);
+
+return $injector;
